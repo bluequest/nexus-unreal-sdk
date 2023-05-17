973d8e811fadfb5751ad32824b6be5fec88aa333
Josh Dowell <josh.dowell.winter@gmail.com>
2023-05-17 18:49:01 +0100

    Add a simple settings class so users can specify their public key in their DefaultGame.ini via the Project Settings dialog (#3)

---
+++ NexusUnrealSDK/Source/NexusUnrealSDK/Public/NexusSettings.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NexusSettings.generated.h"
+
+/**
+ * Nexus SDK plugin settings
+ */
+
+UCLASS(Config = Game, DefaultConfig, meta = (DisplayName = "Nexus SDK"))
+class UNexusUnrealSDKSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	/** Shortcut for GetDefault. */
+	static const UNexusUnrealSDKSettings *Get()
+	{
+		return GetDefault<UNexusUnrealSDKSettings>();
+	}
+
+	UPROPERTY(Config, EditAnywhere, Category = "General", meta = (
+		DisplayName = "Public API Key",
+		ToolTip = "This is your public facing Nexus API key."))
+	FString PublicKey;
+};
