e2bd1a4bd6a4a97fbf8bc151354a0ec0e730c50f
Slartibarty <josh@bullsquid.com>
2023-05-17 02:29:58 +0100

    Add a simple settings class so users can specify their public key in their DefaultGame.ini via the Project Settings dialog

--- NexusUnrealSDK/Source/NexusUnrealSDK/NexusUnrealSDK.Build.cs
+++ NexusUnrealSDK/Source/NexusUnrealSDK/NexusUnrealSDK.Build.cs
@@ -27,6 +27,7 @@ public class NexusUnrealSDK : ModuleRules
 			{
 				"Core",
 				// ... add other public dependencies that you statically link with here ...
+				"DeveloperSettings"
 			}
 			);
 			
